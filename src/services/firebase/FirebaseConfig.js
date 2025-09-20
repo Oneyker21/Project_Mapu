f325281bfebbbb42d1307647a0ab50aee3c0119f
Oneyker21 <oneygaleano21@gmail.com>
2025-09-19 19:59:04 -0600

    conexion y credenciales

--- src/services/firebase/FirebaseConfig.js
+++ src/services/firebase/FirebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDr1e5DfQMBrVsu2tiv1fZhU0UxAtE8GCI",
-  authDomain: "mapu-1e852.firebaseapp.com",
-  projectId: "mapu-1e852",
-  storageBucket: "mapu-1e852.firebasestorage.app",
-  messagingSenderId: "290493369828",
-  appId: "1:290493369828:web:01577a414511287e7f8bf1",
-  measurementId: "G-TWBQXP2518"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
