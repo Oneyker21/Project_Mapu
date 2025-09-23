2acd50c850447c09cf70a4dc7aa8b073dfb01304
Oneyker21 <oneygaleano21@gmail.com>
2025-09-23 09:01:49 -0600

    Cambio de estructura

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { LoginScreen } from './src/screens/Auth';
+import { Map } from './src/components/map/Map';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Map />;
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
